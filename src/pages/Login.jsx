64c4d19deaabd2223b04104378ca9ca1ab2a49f2
LudwingValecillos <ludwingvaldev@gmail.com>
2024-09-21 00:53:28 -0300

    a

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
 
   return (
     <section className="bg-white">
-      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+      <div className="lg:grid lg:min-h-screen  lg:grid-cols-12">
         <section className="relative flex items-end h-32 bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
           <img
             alt=""
@@ -73,7 +73,7 @@ const Login = () => {
 
         <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
           <div className="max-w-xl lg:max-w-3xl">
-            <form onSubmit={handleLogin} className="flex flex-col gap-4 w-96">
+            <form onSubmit={handleLogin} className="flex flex-col px-4 lg:px-0 gap-4 w-96">
               <h2 className="text-5xl text-center">Login</h2>
               <LabelInput
                 type="email"
