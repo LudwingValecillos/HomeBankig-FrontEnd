c1687a54e96be02d7bac966ff7db2cb36460f4f1
LudwingValecillos <ludwingvaldev@gmail.com>
2024-09-22 21:14:06 -0300

    a

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Account = (props) => {
   return (
     <div className="">
-      <div className="Card   bg-slate-200 p-10  sm:text-xl lg:text-2xl border-blue-800 border-4  rounded-xl flex flex-col flex-wrap gap-4 shadow-xl hover:border-8 hover:shadow-2xl hover:text-3xl duration-300 bg-cover bg-center">
+      <div className="Card   bg-slate-200 p-10  sm:text-xl lg:text-2xl border-blue-800 border-4  rounded-xl flex flex-col flex-wrap gap-4 shadow-xl lg:hover:border-8 lg:hover:shadow-2xl hover:text-3xl duration-300 bg-cover bg-center">
         <p>Account Number: {props.account}</p>
         <p>Balance: ${props.balance.toLocaleString()},00</p>
         <p>Opening Date: {props.openingDate}</p>
