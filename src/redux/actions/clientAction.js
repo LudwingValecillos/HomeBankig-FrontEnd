1b6e6da33946576b1ca1fda65bdc4b251ce17c3d
LudwingValecillos <ludwingvaldev@gmail.com>
2024-09-22 21:15:24 -0300

    a

--- src/redux/actions/clientAction.js
+++ src/redux/actions/clientAction.js
@@ -55,59 +55,59 @@
 //   }
 // );
 
-// import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
+import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-// // Acción asincrónica para cargar el cliente
-// export const loadClient = createAsyncThunk(
-//   "loadClient",
-//   async (_, { rejectWithValue }) => {
-// const token = localStorage.getItem("token");
+// Acción asincrónica para cargar el cliente
+export const loadClient = createAsyncThunk(
+  "loadClient",
+  async (_, { rejectWithValue }) => {
+const token = localStorage.getItem("token");
 
-//     try {
-//       const response = await axios.get(
-//         "https://homebankig.onrender.com/api/auth/current",
-//         {
-//           headers: { Authorization: `Bearer ${token}` },
-//         }
-//       );
-//       return response.data; // Devuelve los datos del cliente directamente
-//     } catch (error) {
-//       console.error("Error loading client:", error);
-//       return rejectWithValue(
-//         error.response ? error.response.data : "Unknown error"
-//       ); // Devuelve un mensaje de error
-//     }
-//   }
-// );
+    try {
+      const response = await axios.get(
+        "https://homebankig.onrender.com/api/auth/current",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      return response.data; // Devuelve los datos del cliente directamente
+    } catch (error) {
+      console.error("Error loading client:", error);
+      return rejectWithValue(
+        error.response ? error.response.data : "Unknown error"
+      ); // Devuelve un mensaje de error
+    }
+  }
+);
 
-// // Acción para agregar una tarjeta al cliente
-// export const addCardToClient = createAction("addCardToClient", (newCard) => {
-//   return {
-//     payload: newCard,
-//   };
-// });
+// Acción para agregar una tarjeta al cliente
+export const addCardToClient = createAction("addCardToClient", (newCard) => {
+  return {
+    payload: newCard,
+  };
+});
 
-// export const addAccountToClient = createAsyncThunk(
-//   "addCardToClient",
-//   async (_, { rejectWithValue }) => {
-// const token = localStorage.getItem("token");
+export const addAccountToClient = createAsyncThunk(
+  "addCardToClient",
+  async (_, { rejectWithValue }) => {
+const token = localStorage.getItem("token");
 
-//     try {
-//       const response = await axios.post(
-//         "https://homebankig.onrender.com/api/accounts/clients/current/accounts", token,
-//         {
-//           headers: { Authorization: `Bearer ${token}` },
-//         }
-//       );
-//       console.log(response.data);
+    try {
+      const response = await axios.post(
+        "https://homebankig.onrender.com/api/accounts/clients/current/accounts", token,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      console.log(response.data);
       
-//       return response.data; // Devuelve los datos del cliente directamente
-//     } catch (error) {
-//       console.error("Error loading client:", error);
-//       return rejectWithValue(
-//         error.response ? error.response.data : "Unknown error"
-//       ); // Devuelve un mensaje de error
-//     }
-//   }
-// );
+      return response.data; // Devuelve los datos del cliente directamente
+    } catch (error) {
+      console.error("Error loading client:", error);
+      return rejectWithValue(
+        error.response ? error.response.data : "Unknown error"
+      ); // Devuelve un mensaje de error
+    }
+  }
+);
